5468b50156462489f4906d87725db676b5863e7f
shalinisheetal <shalinisheetal2001@gmail.com>
2023-07-02 11:08:14 +0530

    Fetch from api

--- src/components/News.js
+++ src/components/News.js
@@ -2,20 +2,39 @@ import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 
 export class News extends Component {
+  articles = [];
+  constructor() {
+    super();
+    this.state = {
+      articles: this.articles,
+      loading: false,
+    };
+  }
+  async componentDidMount() {
+    let url =
+      "https://newsapi.org/v2/top-headlines?country=in&apiKey=21a14d027e7f4247a13b4f8780344152";
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState((this.articles = parsedData.articles));
+  }
   render() {
     return (
       <div className="container my-3">
         <h2>NewsPanda - Top Headlines</h2>
+
         <div className="row">
-          <div className="col-md-4">
-            <NewsItem title="this is news title" description={"news desc"} />
-          </div>
-          <div className="col-md-4">
-            <NewsItem title={"this is news title"} description={"news desc"} />
-          </div>
-          <div className="col-md-4">
-            <NewsItem title={"this is news title"} description={"news desc"} />
-          </div>
+          {this.articles.map((article) => {
+            return (
+              <div className="col-md-4" key={article.url}>
+                <NewsItem
+                  title={article.title}
+                  description={article.description}
+                  newsUrl={article.url}
+                  imageUrl={article.urlToImage}
+                />
+              </div>
+            );
+          })}
         </div>
       </div>
     );
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,16 +2,25 @@ import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
-    let { title, description } = this.props;
+    let { title, description, imageUrl, newsUrl } = this.props;
     return (
-      <div>
+      <div className="my-3">
         <div className="card" style={{ width: "18rem" }}>
-          <img src="..." className="card-img-top" alt="..." />
+          <img
+            src={
+              imageUrl
+                ? imageUrl
+                : "https://images.rawpixel.com/image_png_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvam9iNjcwLTAwOTgtcC1sMWJqMGZ0eS5wbmc.png?s=VKfm-YQmU9a1qFlH-HbWPwUy-DG9dM7ZTq1G73m85hs"
+            }
+            className="card-img-top"
+            alt="..."
+            style={{ width: "286px", height: "161px", objectFit: "cover" }}
+          />
           <div className="card-body">
-            <h5 className="card-title">{title}</h5>
-            <p className="card-text">{description}</p>
-            <a href="/" className="btn btn-primary">
-              Go somewhere
+            <h5 className="card-title truncate">{title}</h5>
+            <p className="card-text  truncate">{description}</p>
+            <a href={newsUrl} className="btn btn-sm btn-primary">
+              Read More
             </a>
           </div>
         </div>
